004e95063b694365fd21d9a933e343200ac31970
SacredDiablo <56549314+SacredDiablo@users.noreply.github.com>
2020-01-20 13:48:33 +0300

    Update Game of life.py

--- Life/Game of life.py
+++ Life/Game of life.py
@@ -1,20 +1,11 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fileencoding=utf-8
 import numpy as np
 import time
 import sys
 
-
-# In[30]:
-
-
+#выбор способа ввода массива. 1 - генерация, 2 - из файла
 if int(sys.argv[1]) == 1:
     m = int(input('Введите M:'))
     n = int(input('Введите N:'))
@@ -27,19 +18,17 @@ if int(sys.argv[1]) == 2:
     m = len(array)
     n = len(array[0])
 
-
-# In[27]:
-
-
 t=time.time()
 a = True
 z = 0
+
 while a == True:
     z+=1
     count = 0
     new_array = np.zeros((m,n), np.int)
     for i in range(0,m):
         for j in range(0,n):
+            #проверка условий на край поля. Если край, берутся только ближайшие значения (другой конец поля не учитывается)
             if i == 0 and j == 0:
                 count = int( array[i+1][j] + array[i+1][j+1]+ array[i][j+1])
             elif i == m - 1 and j == n - 1:
@@ -49,18 +38,24 @@ while a == True:
             elif i == m-1 and j == 0:
                     count = int( array[i-1][j] + array[i-1][j+1]+ array[i][j+1])
             elif i == 0:
-                count = int(array[i+1][j-1] + array[i+1][j] + array[i+1][j+1]                        + array[i][j-1] + array[i][j+1])
-
+                count = int(array[i+1][j-1] + array[i+1][j] + array[i+1][j+1]\                        
+                            + array[i][j-1] + array[i][j+1])
             elif i == m-1:
-                count = int(array[i-1][j-1] + array[i-1][j] + array[i-1][j+1]                        + array[i][j-1] + array[i][j+1])
+                count = int(array[i-1][j-1] + array[i-1][j] + array[i-1][j+1]\                       
+                            + array[i][j-1] + array[i][j+1])
             elif j == 0:
-                count = int(array[i-1][j] + array[i-1][j+1]                        + array[i+1][j] + array[i+1][j+1]                        + array[i][j+1])
+                count = int(array[i-1][j] + array[i-1][j+1]\                        
+                            + array[i+1][j] + array[i+1][j+1]\                        
+                            + array[i][j+1])
             elif j == n-1:
-                count = int(array[i-1][j-1] + array[i-1][j]                        + array[i+1][j-1] + array[i+1][j]                        + array[i][j-1])        
-    
+                count = int(array[i-1][j-1] + array[i-1][j]\                        
+                            + array[i+1][j-1] + array[i+1][j]\                        
+                            + array[i][j-1])            
             elif i>0 and j>0 and i<m-1 and j<n-1:
-                count = int(array[i-1][j-1] + array[i-1][j] + array[i-1][j+1]                    + array[i+1][j-1] + array[i+1][j] + array[i+1][j+1]                    + array[i][j-1] + array[i][j+1])
-          
+                count = int(array[i-1][j-1] + array[i-1][j] + array[i-1][j+1]\
+                            + array[i+1][j-1] + array[i+1][j] + array[i+1][j+1]\
+                            + array[i][j-1] + array[i][j+1])
+#изменение поля в зависимости от условий          
             if array[i][j] == 0:
                 if count == 3:
                     new_array[i][j] = 1
@@ -70,6 +65,7 @@ while a == True:
                     new_array[i][j]=0
                 elif count == 2 or count == 3:
                     new_array[i][j]=array[i][j]
+#выход из цикла при достижении лучшего результата, посекундный вывод массива на экран    
     if np.array_equal(array, new_array) == False:
         array = new_array               
         print ('Iteration: ', z)
